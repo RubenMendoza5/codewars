2a6606567422dbcbc9f8c607cc16854716c7a700
Ruben Mendoza <ruben.mendoza@bo.truextend.com>
2020-09-03 10:20:21 -0400

    first commit

--- src/main/java/codewars/k8/DalmatiansSquashTheBugs.java
+++ src/main/java/codewars/k8/DalmatiansSquashTheBugs.java
@@ -1,2 +1,29 @@
-package codewars.k8;public class DalmatiansSquashTheBugs {
+package codewars.k8;
+
+/**
+ * Your friend has been out shopping for puppies (what a time to be alive!)...
+ * He arrives back with multiple dogs, and you simply do not know how to respond!
+ *
+ * By repairing the function provided, you will find out exactly how you should respond,
+ * depending on the number of dogs he has.
+ *
+ * The number of dogs will always be a number and there will always be at least 1 dog.
+ */
+
+public class DalmatiansSquashTheBugs {
+    public static String howManyDalmatians(int number) {
+        String[] dogs = {"Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"};
+
+        String respond = number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number == 101 ? dogs[3] : dogs[2]));
+
+        return respond;
+    }
 }
+
+/**
+ * static String howManyDalmatians(int number) {
+ *     return number <= 10 ? "Hardly any" :
+ *            number <= 50 ? "More than a handful!" :
+ *            number == 101 ? "101 DALMATIANS!!!" : "Woah that's a lot of dogs!";
+ *   }
+ */
--- src/test/java/codewars/k8/DalmatiansSquashTheBugsTest.java
+++ src/test/java/codewars/k8/DalmatiansSquashTheBugsTest.java
@@ -1,4 +1,18 @@
+package codewars.k8;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class DalmatiansSquashTheBugsTest {
-  
+
+    @Test
+    public void basicTests() {
+        assertEquals("More than a handful!",DalmatiansSquashTheBugs.howManyDalmatians(26));
+        assertEquals("Hardly any" ,DalmatiansSquashTheBugs.howManyDalmatians(8));
+        assertEquals("More than a handful!" ,DalmatiansSquashTheBugs.howManyDalmatians(14));
+        assertEquals("Woah that's a lot of dogs!" ,DalmatiansSquashTheBugs.howManyDalmatians(80));
+        assertEquals("Woah that's a lot of dogs!" ,DalmatiansSquashTheBugs.howManyDalmatians(100));
+        assertEquals("101 DALMATIANS!!!" ,DalmatiansSquashTheBugs.howManyDalmatians(101));
+    }
 }
